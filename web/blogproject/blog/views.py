003246707657a822ca70ed8affbb5c460b1fbb04
Gary Tang <13122260573@163.com>
2018-10-15 17:28:13 +0800

    Web学习

---
+++ web/blogproject/blog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Post
+
+
+def index(request):
+    post_list = Post.objects.all().order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
